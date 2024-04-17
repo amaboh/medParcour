6038bbbb77bdcc91bb15365c6512af1a4077c8be
Fangmbeng <yorobeau@gmail.com>
2024-04-17 14:28:14 -0600

    Vertex Ai intergrated, Twilio outgoing call, Email module added to send generate reports and transcription module using Google voice to text

--- sockets.py
+++ sockets.py
@@ -5,14 +5,12 @@ import logging
 from flask import Flask, render_template
 from flask_sock import Sock
 from twilio.rest import Client
-from twilio.twiml.voice_response import VoiceResponse
 from langchain_google_genai import ChatGoogleGenerativeAI
 from transcription import SpeechClientBridge
 from google.cloud.speech import RecognitionConfig, StreamingRecognitionConfig
 import threading
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_vertexai import ChatVertexAI
-
+import vertexai
+from vertexai.language_models import ChatModel, InputOutputTextPair
 
 
 app = Flask(__name__)
@@ -22,14 +20,14 @@ HTTP_SERVER_PORT = 5000
 TIWILIO_SAMPLE_RATE = 800  # Hz
 
 # Twilio configuration
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ('TWILIO_ACCOUNT_SID')
+auth_token = os.environ('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
-TWILIO_NUMBER=os.environ['TWILIO_NUMBER']
+TWILIO_NUMBER=os.environ('TWILIO_NUMBER')
 
 # Google configuration
-GOOGLE_PROJECT_ID = os.environ['GOOGLE_PROJECT_ID']
-GOOGLE_API_KEY=os.environ['GOOGLE_API_KEY']
+GOOGLE_PROJECT_ID = os.environ('GOOGLE_PROJECT_ID')
+GOOGLE_API_KEY=os.environ('GOOGLE_API_KEY')
 GOOGLE_LANGUAGE_CODE = 'en-US'
 
 
@@ -47,7 +45,43 @@ config = RecognitionConfig(
     language_code="en-US",
 )
 streaming_config = StreamingRecognitionConfig(config=config, interim_results=True)
+final_report=''
 
+@app.route('/chat')
+def streaming_prediction(
+    project_id: str,
+    location: str,
+    res,
+) -> str:
+    
+    """Streaming Chat Example with a Large Language Model"""
+
+    vertexai.init(project=project_id, location=location)
+
+    chat_model = ChatModel.from_pretrained("chat-bison")
+
+    parameters = {
+        "temperature": 0.8,  # Temperature controls the degree of randomness in token selection.
+        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
+        "top_p": 0.95,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+    }
+
+    chat = chat_model.start_chat(
+        context="My name is Med_Ai. You are an health care assistant, knowledgeable about keeping track of health records.",
+        examples=[
+            InputOutputTextPair(
+                input_text="I]m not feeling fine",
+                output_text="Have you taken your drugs today",
+            ),
+        ],
+    )
+
+    responses = chat.send_message_streaming(
+        message= f"{res}", **parameters)
+    for response in responses:
+        print(response.text)
+        
 
 @app.route("/twiml", methods=["POST"])
 def return_twiml():
@@ -57,6 +91,7 @@ def return_twiml():
 
 def on_transcription_response(response):
     transcription = ""  # Define response2 outside the loop
+    global final_report
 
     if not response.results:
         return
@@ -69,28 +104,9 @@ def on_transcription_response(response):
         if result.is_final:
             transcription = result.alternatives[0].transcript
             print("Transcription: " + transcription)
-            chat(transcription)
-    
+            final_report+=transcription
+            streaming_prediction(GOOGLE_PROJECT_ID, 'us-central1', transcription)
 
-def chat(results):
-    response1=VoiceResponse()
-    system = "You are a helpful virtual agent who helps keep track of how the patient feels"
-    human_message = f'{results}'
-
-    # Create a ChatPromptTemplate with system and human messages
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", system),
-        ("human", human_message)
-    ])
-
-
-    chat = ChatVertexAI(project=GOOGLE_PROJECT_ID, temperature=0.7)
-
-    chain = prompt | chat
-
-    for chunk in chain.stream({}):
-        print(chunk.content)
-        response1.say(chunk.content)
 
 @sockets.route('/realtime')
 def handle_media(ws):
@@ -133,10 +149,10 @@ def make_call(phone_number="+12403980310"):
     twiml = f"""
     <Response>
         <Say>
-            What is your name?
+            How are you feeling today?
         </Say>
         <Connect>
-            <Stream url="wss://86ec-2601-282-1d80-7b00-9d88-8f2b-fc90-fd27.ngrok-free.app/realtime" />
+            <Stream url="wss://13a2-2601-282-1d80-7b00-1561-4736-b44f-b6f0.ngrok-free.app/realtime" />
         </Connect>
     </Response>
     """.strip()
@@ -147,9 +163,11 @@ def make_call(phone_number="+12403980310"):
         from_=TWILIO_NUMBER,
         to=phone_number,
     )
+    
     return str(call.sid)
 
 make_call("+12403980310")  # Uncomment this line to make the call when the application is run
+
 
 if __name__ == '__main__':
     app.logger.setLevel(logging.DEBUG)
